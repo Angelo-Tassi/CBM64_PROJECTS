fa525244c07e9f9d699b7f108d5a1b0684932d99
Angelo Tassi <angelotax@gmail.com>
2025-02-04 16:34:28 +0100

    sys 49152 Prints Hello World

--- Helloworld/helloworld.asm
+++ Helloworld/helloworld.asm
@@ -1,12 +1,12 @@
-                .encoding "petscii_upper"
-                * = $c000
-                ldx #$00
-loop:           lda htext,x
-                beq done
-                jsr $ffd2
-                inx
-                jmp loop
-done:           RTS
+        .encoding "petscii_upper"
+        * = $c000
+        ldx #$00
+loop:   lda htext,x
+        beq done
+        jsr $ffd2
+        inx
+        jmp loop
+done:   rts
 
-.htext:         .text "HELLO WORLD"
-                .byte 0
+htext: .text "HELLO WORLD"
+        .byte 0
